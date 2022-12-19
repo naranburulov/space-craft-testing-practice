ee37f914d3a74738fe2137a382282616a980f696
naranburulov <naranburulov@gmail.com>
2022-12-19 11:50:44 -0500

    Initial commit

---
+++ src/test/java/com/cydeo/spacecraft/service/impl/CreatePlayerServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.cydeo.spacecraft.service.impl;
+
+import com.cydeo.spacecraft.dto.CreateGameDTO;
+import com.cydeo.spacecraft.entity.Player;
+import com.cydeo.spacecraft.enumtype.Boost;
+import com.cydeo.spacecraft.enumtype.Level;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreatePlayerServiceImplTest {
+
+    private CreatePlayerServiceImpl createPlayerService;
+
+    @BeforeEach
+    public void setUp(){
+        createPlayerService = new CreatePlayerServiceImpl();
+    }
+
+
+    @Test
+    public void should_create_player_with_big_bomb_boost_type_and_level_easy(){
+
+        //given
+        CreateGameDTO createGameDTO = new CreateGameDTO();
+        createGameDTO.setBoost(Boost.BIG_BOMB);
+        createGameDTO.setLevel(Level.EASY);
+        createGameDTO.setUsername("username");
+
+        //when
+        Player player = createPlayerService.createPlayer(createGameDTO);
+
+        //then
+        assertEquals(player.getHealth(), 2000);
+        assertEquals(player.getArmor(), 7);
+        assertEquals(player.getShootPower(), 5010);
+
+
+
+    }
+
+}
